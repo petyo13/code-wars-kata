4f2572d3e06eca2cf83d844a90fb495c84ee99ba
petyo13 <45045277+petyo13@users.noreply.github.com>
2019-05-23 16:19:27 +0300

    Create index.js

---
+++ Grasshopper - Messi goals function/index.js
@@ -0,0 +1,10 @@
+/*Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions.
+Complete the goals function to return his total goals (the sum) for all three leagues.*/
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+}
