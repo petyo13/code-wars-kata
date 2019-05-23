b7ff33b0ed4100c33e83e1d13afd2137eed177fc
petyo13 <45045277+petyo13@users.noreply.github.com>
2019-05-23 15:57:21 +0300

    Update index.js

--- How many stairs will Suzuki climb in 20 years?/index.js
+++ How many stairs will Suzuki climb in 20 years?/index.js
@@ -1,3 +1,7 @@
+/*Suzuki is a monk who climbs a large staircase to the monastery as part of a ritual. Some days he climbs more stairs than others depending on the number of students he must train in the morning. He is curious how many stairs might be climbed over the next 20 years and has spent a year marking down his daily progress.
+
+The sum of all the stairs logged in a year will be used for estimating the number he might climb in 20.*/
+
 function stairsIn20(s){
   var mapped = s.map(dayStairs);
   var result = 0;
