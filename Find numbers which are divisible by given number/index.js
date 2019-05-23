45db5125d46915c46d9c1cb30d904dbdc53e1b78
petyo13 <45045277+petyo13@users.noreply.github.com>
2019-05-23 15:53:54 +0300

    Create index.js

---
+++ Find numbers which are divisible by given number/index.js
@@ -0,0 +1,10 @@
+/*Complete the function which takes two arguments and returns all numbers
+ which are divisible by the given divisor.
+ First argument is an array of numbers and the second is the divisor.*/
+
+function divisibleBy(numbers, divisor) {
+  var result = numbers.filter(function(num) {
+    return num % divisor === 0;
+  });
+  return result;
+}
